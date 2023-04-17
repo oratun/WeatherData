083a4afc96bccca3c8e8127d18a0b303f45d61c9
zh.tong <zh.tong@idiaoyan.com>
2023-04-17 23:18:54 +0800

    add shell script for sg weather

--- sg/sg.sh
+++ sg/sg.sh
@@ -1,5 +1,9 @@
 # !/bin/sh
+# pollutant
 cd /opt/AirHongKong
 source venv/bin/activate
 which python3
 python3 -m sg.fetch
+# weather visibility
+source venv38/bin/activate
+python3 -m sg.observation
