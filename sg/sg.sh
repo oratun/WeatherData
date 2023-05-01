0c22b28de0f239b3696da11271de78f9e9acee50
oratun <warn1667@hotmail.com>
2023-05-01 14:18:11 +0800

    sg.sh now using docker-compose

--- sg/sg.sh
+++ sg/sg.sh
@@ -1,9 +1,3 @@
 # !/bin/sh
-# pollutant
 cd /opt/weatherdata
-source venv/bin/activate
-which python3
-python3 -m sg.fetch
-# weather visibility
-source venv38/bin/activate
-python3 -m sg.observation
+docker-compose up -d
